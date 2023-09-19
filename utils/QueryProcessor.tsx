1e20d8c8420e04f6a512493d6145d5e33542d370
Matthew Leboffe <matthew8853@gmail.com>
2023-09-19 15:57:05 -0400

    name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -11,7 +11,7 @@ export default function QueryProcessor(query: string): string {
     );
   } else if (query.toLowerCase().includes("what is your name?")) {
     return (
-      "My name is Lebo."
+      "TheHolySpirit"
     );
   } else if (query.toLowerCase().includes("plus")) {
     const numbers = query.match(/\d+/g); // Extract numbers from string
